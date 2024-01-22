8940d4ab46c0e6cfe5ddc3d3c1851a8e55d74c80
sh <sandrahammamitlili@gmail.com>
2024-01-22 17:29:03 +0100

    my-first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgChartsModule } from 'ng2-charts';
+import {NavComponent} from './nav/nav.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgChartsModule,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
